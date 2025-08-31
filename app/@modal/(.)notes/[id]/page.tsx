f44461256b7ae49e5fbff72c0bc15cb12201b104
ARTEM POBEHAILO <partemishe@gmail.com>
2025-08-31 12:10:28 +0300

    di

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -8,13 +8,13 @@ import NotePreview from "./NotePreview.client";
 import { getSingleNote } from "@/lib/api";
 
 interface NotePreviewPageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default async function NotePreviewPage({
   params,
 }: NotePreviewPageProps) {
-  const { id } = params;
+  const { id } = await params;
 
   const queryClient = new QueryClient();
 
